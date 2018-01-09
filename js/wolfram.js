d2e4ec6508ff51fb946dcf93a71317537e119a25
Akshay Kore <akkore@Kores-Mac.guest.corp.microsoft.com>
2018-01-09 19:04:14 +0530

    ddd

--- js/wolfram.js
+++ js/wolfram.js
@@ -2,10 +2,10 @@ function getwolfram(input){
         clearall();
         play_magic_sound();
         //alert("dodo");
-        var wolframapicall = "http://api.wolframalpha.com/v1/spoken?appid=G5XHT5-5EW648YL5Y&i=" + input + "&units=metric";
-        
+        var wolframapicall = "https://api.wolframalpha.com/v1/spoken?appid=G5XHT5-5EW648YL5Y&i=" + input + "&units=metric";
+
         $.get(wolframapicall, wolframcallback);
-        
+
         function wolframcallback(wolframans){
             console.log("success");
             //alert(wolframans);
@@ -14,11 +14,10 @@ function getwolfram(input){
             var mirror_answer = document.getElementById("mirroranswer");
                 mirror_answer.innerHTML = wolframans;
         }
-        
+
         setTimeout(clearall, 60000);
 }
 
 
-    
+
 //getwolfram("define information");
-    
