d0427fb709e23c46c4db25e562f6699c6e236b92
Akshay Kore <akkore@Kores-Mac.guest.corp.microsoft.com>
2018-01-09 19:07:36 +0530

    hh

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,22 @@
 function getweather() {
-  
-var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=Hyderabad&appid=a7689f15092d8a5c1185dedbf0d3b998';
+
+var apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=Hyderabad&appid=a7689f15092d8a5c1185dedbf0d3b998';
 
 
 
 $.getJSON(apiCall, weatherCallback);
 
 function weatherCallback(weatherdata){
-    
+
     var temperature = Math.round(weatherdata.main.temp - 273.15);
     //alert(temperature + " °C");
     console.log(weatherdata.main);
     //console.log("lol");
     var weather_element = document.getElementById("weather");
         weather_element.innerHTML = temperature + " °C";
-    
+
 }
 }
-    
+
 getweather();
 setInterval(getweather, 60000);
